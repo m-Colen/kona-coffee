2196ddf5bc859b47f7df8285d8226eb965b03819
m-colen <mzcolen@gmail.com>
2022-06-27 22:14:37 -0500

    Initial nav built

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@
 
 const hamMenu = document.querySelector(".hamburger-menu"); // Ham Menu Button
 
-// Toggles ham menu button display
+// Toggles ham menu display
 
 const toggleHamMenuDisplay = () => {
   const hamMenuIcon = document.querySelector("#ham-menu-icon");
   const hamMenuClose = document.querySelector("#ham-menu-close");
+  const hamMenuDisplay = document.querySelector(".mobile-nav-menu");
   hamMenuIcon.classList.toggle("hidden");
   hamMenuClose.classList.toggle("hidden");
+  hamMenuDisplay.classList.toggle("block");
+  hamMenuDisplay.classList.toggle("slide-in-right");
 };
 
 hamMenu.addEventListener("click", toggleHamMenuDisplay);
