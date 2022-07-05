a78d84bb13514ff53f3391d163f93b630b8a83f9
m-colen <mzcolen@gmail.com>
2022-07-05 00:02:47 -0500

    Main cards built/Initial footer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* Hamburger menu interactivity */
+/********** Hamburger menu interactivity **********/
 
 const hamMenu = document.querySelector(".hamburger-menu"); // Ham Menu Button
 
@@ -12,6 +12,32 @@ const toggleHamMenuDisplay = () => {
   hamMenuClose.classList.toggle("hidden");
   hamMenuDisplay.classList.toggle("block");
   hamMenuDisplay.classList.toggle("slide-in-right");
+  toggleScrollBar();
+};
+
+// Locks scroll when mobile nav menu is open
+
+const toggleScrollBar = () => {
+  const body = document.body;
+  body.classList.toggle("overflow-hidden");
 };
 
 hamMenu.addEventListener("click", toggleHamMenuDisplay);
+
+/**********Footer **********/
+
+// Toggles the display of an element
+const toggleDisplay = (elem) => elem.classList.toggle("hidden");
+
+const arrowButtons = document.querySelectorAll(".fa-angle-down");
+
+arrowButtons.forEach((elem) => {
+  switch (elem) {
+    case elem.className === "fa-solid fa-angle-down about-link":
+      elem.addEventListener("click", () => {
+        const aboutHidden = document.querySelector("hidden-menu-about");
+        toggleDisplay(aboutHidden);
+        console.log(aboutHidden);
+      });
+  }
+});
