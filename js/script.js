0ce4e8acbc48608fd10a0657ab37acfcef39a397
m-colen <mzcolen@gmail.com>
2022-07-05 21:50:25 -0500

    Initial footer section

--- js/script.js
+++ js/script.js
@@ -26,18 +26,63 @@ hamMenu.addEventListener("click", toggleHamMenuDisplay);
 
 /**********Footer **********/
 
+// Targets
+const aboutButton = document.querySelector(".about-clickable");
+const careersButton = document.querySelector(".careers-clickable");
+const socialImpactButton = document.querySelector(".social-impact-clickable");
+const businessPartnersButton = document.querySelector(
+  ".business-partners-clickable"
+);
+const orderButton = document.querySelector(".order-clickable");
+
 // Toggles the display of an element
-const toggleDisplay = (elem) => elem.classList.toggle("hidden");
+const toggleDisplay = (elem) => {
+  elem.classList.toggle("hidden");
+};
 
-const arrowButtons = document.querySelectorAll(".fa-angle-down");
+// Rotates an element 180deg
+const rotate180 = (elem) => elem.classList.toggle("rotate-180");
 
-arrowButtons.forEach((elem) => {
-  switch (elem) {
-    case elem.className === "fa-solid fa-angle-down about-link":
-      elem.addEventListener("click", () => {
-        const aboutHidden = document.querySelector("hidden-menu-about");
-        toggleDisplay(aboutHidden);
-        console.log(aboutHidden);
-      });
-  }
-});
+// Toggles hidden menu and rotates the arrow icons
+const toggleAbout = (elem, icon) => {
+  toggleDisplay(elem);
+  rotate180(icon);
+};
+
+// Event listeners for footer accordions (set at less than 750px window width)
+
+if (window.innerWidth < 750) {
+  aboutButton.addEventListener("click", () => {
+    const aboutHidden = document.querySelector(".about-hidden");
+    const aboutArrow = document.querySelector(".about-arrow");
+    toggleAbout(aboutHidden, aboutArrow);
+  });
+
+  careersButton.addEventListener("click", () => {
+    const careersHidden = document.querySelector(".careers-hidden");
+    const careersArrow = document.querySelector(".careers-arrow");
+    toggleAbout(careersHidden, careersArrow);
+  });
+
+  socialImpactButton.addEventListener("click", () => {
+    const socialImpactHidden = document.querySelector(".social-impact-hidden");
+    const socialImpactArrow = document.querySelector(".social-impact-arrow");
+    toggleAbout(socialImpactHidden, socialImpactArrow);
+  });
+
+  businessPartnersButton.addEventListener("click", () => {
+    const businessPartnersHidden = document.querySelector(
+      ".business-partners-hidden"
+    );
+    const businessPartnersArrow = document.querySelector(
+      ".business-partners-arrow"
+    );
+    toggleAbout(businessPartnersHidden, businessPartnersArrow);
+  });
+
+  orderButton.addEventListener("click", () => {
+    const orderHidden = document.querySelector(".order-hidden");
+    const orderArrow = document.querySelector(".order-arrow");
+    toggleAbout(orderHidden, orderArrow);
+  });
+}
